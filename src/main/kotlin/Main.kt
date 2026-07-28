d13328636e4de27727b59ef1e09569d327711623
Douglas Jasper <doug.jasper@trios.com>
2026-07-28 09:33:06 -0400

    Finds shortest word

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,14 +3,25 @@ package org.example
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    println("String Examples 2026")
+    println()
+
+    val sentence = "kotlin makes coding fun and efficient"
+    val shortest = findShortestWord(sentence)
+
+    println("Shortest word: $shortest")
+}
+
+fun findShortestWord(input: String): String? {
+    // Split on any non-letter character (spaces, punctuation, etc.)
+    val words = input
+        .split(Regex("[^A-Za-z]+"))
+        .filter { it.isNotEmpty() }
+
+    if (words.isEmpty()) {
+        return null
     }
+
+    return words.minByOrNull { it.length }
 }
